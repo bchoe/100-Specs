c00698845960063f7de8aa27808f01e1dbb21d78
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-22 21:19:36 -1000

    Meal

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1107,6 +1107,9 @@ function Cookie(flavor){
  * @param {Array} foods All the foods in the meal
  */
 
+function Meal(foods){
+  this.foods = foods;
+}
 
 /**
  * Create a new instance of the Classes you defined above, below!
