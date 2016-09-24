e591ff4020c9b939ae2638f746f3dfe083c55e4c
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-23 19:32:14 -1000

    commit

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1280,7 +1280,15 @@ Box.prototype.openBox = function(){
  * Return true if openClose opens the door, false if openClose closes the door.
  *
  */
-
+Door.prototype.openClose = function(){
+  if(this.isOpen === false){
+    this.isOpen = true;
+    return true;
+  } else {
+    this.open = false;
+    return false;
+  }
+};
 
 /* Step 86
  *
